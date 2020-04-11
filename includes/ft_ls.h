504e1c6db1e755300915dca46340bfdf555b7d44
sadawi <hahli144@hotmail.com>
2020-04-11 17:57:23 +0300

    add function prototypes, dir struct

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/10 12:20:24 by sadawi            #+#    #+#             */
-/*   Updated: 2020/04/10 21:45:16 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/04/11 17:50:22 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@
 # include <pwd.h>
 # include <grp.h>
 
+# define VALID_FLAGS "alrRt"
+
 enum				e_sorting_mode
 {
 	SORT_ALPHA,
 	SORT_ALPHA_REV,
-	SORT_MOD_TIME
+	SORT_MOD_TIME,
+	SORT_MOD_TIME_REV
 };
 
 typedef struct	s_file
@@ -34,5 +37,55 @@ typedef struct	s_file
 	struct s_file	*next;
 	struct stat		stats;
 }				t_file;
+
+typedef struct	s_dir
+{
+	char			*path;
+	t_file			*files;
+	struct stat		stats;
+	struct s_dir	*next;
+}				t_dir;
+
+typedef	struct	s_ls
+{
+	char		*flags;
+	t_file		*files;
+	t_dir		*dirs;
+	int			sort_mode;
+}				t_ls;
+
+void	handle_error(char *message);
+
+int		char_index(char *str, char c);
+
+void	new_file(t_file **files, char *name);
+
+char	*format_time(char *time);
+
+void	open_files(t_ls *ls, t_file *files);
+
+void	add_dir(t_dir **dir, char *path);
+
+int		ft_strcmp_case(const char *s1, const char *s2);
+
+void	save_flag(t_ls *ls, char *flag);
+
+void	save_options(t_ls *ls, int *argc, char ***argv);
+
+void	sort_mode(t_ls *ls);
+
+void	save_files(t_ls *ls, int argc, char **argv);
+
+void	init_ls(t_ls **ls, int *argc, char **argv);
+
+t_file	*sorted_merge(t_ls *ls, t_file *first_half, t_file *second_half);
+
+void	split_list(t_file *head, t_file **first_half, t_file **second_half);
+
+void	mergesort(t_ls *ls, t_file **files);
+
+t_file	*sorted_merge_alpha(t_file *first_half, t_file *second_half);
+
+t_file	*sorted_merge_alpha_rev(t_file *first_half, t_file *second_half);
 
 #endif
