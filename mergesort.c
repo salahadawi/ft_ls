3efb768b2d42695bc4ef2779d21e4cf8c236357a
sadawi <hahli144@hotmail.com>
2020-04-11 17:56:28 +0300

    add output sorting algorithms

---
+++ mergesort.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mergesort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/11 15:38:13 by sadawi            #+#    #+#             */
+/*   Updated: 2020/04/11 16:10:49 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/ft_ls.h"
+
+t_file	*sorted_merge(t_ls *ls, t_file *first_half, t_file *second_half)
+{
+	if (ls->sort_mode == SORT_ALPHA)
+		return (sorted_merge_alpha(first_half, second_half));
+	if (ls->sort_mode == SORT_ALPHA_REV)
+		return (sorted_merge_alpha_rev(first_half, second_half));
+	else
+		handle_error("Invalid sort mode");
+	return NULL;
+}
+
+void	split_list(t_file *head, t_file **first_half, t_file **second_half)
+{
+	t_file *fast;
+	t_file *slow;
+
+	slow = head;
+	fast = head->next;
+	while (fast)
+	{
+		fast = fast->next;
+		if (fast)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+	*first_half = head;
+	*second_half = slow->next;
+	slow->next = NULL;
+}
+
+void	mergesort(t_ls *ls, t_file **files)
+{
+	t_file *head;
+	t_file *first_half;
+	t_file*second_half;
+
+	head = (*files);
+	if (!head || !head->next)
+		return ;
+	split_list(head, &first_half, &second_half);
+	mergesort(ls, &first_half);
+	mergesort(ls, &second_half);
+	*files = sorted_merge(ls, first_half, second_half);
+}
---
+++ sorting_algorithms.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting_algorithms.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/11 15:37:31 by sadawi            #+#    #+#             */
+/*   Updated: 2020/04/11 15:37:56 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/ft_ls.h"
+
+t_file	*sorted_merge_alpha(t_file *first_half, t_file *second_half)
+{
+	t_file *sorted;
+
+	if (!first_half)
+		return (second_half);
+	if (!second_half)
+		return (first_half);
+	if (ft_strcmp_case(first_half->name, second_half->name) < 0)
+	{
+		sorted = first_half;
+		sorted->next = sorted_merge_alpha(first_half->next, second_half);
+	}
+	else
+	{
+		sorted = second_half;
+		sorted->next = sorted_merge_alpha(first_half, second_half->next);
+	}
+	return (sorted);
+}
+
+t_file	*sorted_merge_alpha_rev(t_file *first_half, t_file *second_half)
+{
+	t_file *sorted;
+
+	if (!first_half)
+		return (second_half);
+	if (!second_half)
+		return (first_half);
+	if (ft_strcmp_case(first_half->name, second_half->name) > 0)
+	{
+		sorted = first_half;
+		sorted->next = sorted_merge_alpha_rev(first_half->next, second_half);
+	}
+	else
+	{
+		sorted = second_half;
+		sorted->next = sorted_merge_alpha_rev(first_half, second_half->next);
+	}
+	return (sorted);
+}
